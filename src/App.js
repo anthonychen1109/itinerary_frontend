6f88393949990745289997ae5b89c4a71b68629d
jtx007 <jamesjacobthomas7@gmail.com>
2018-09-06 14:59:08 -0400

    messing with React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import NavBar from './Components/NavBar'
 import CreateTrip from './Containers/CreateTrip';
 import './Assets/css/styles.css';
+import {Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <CreateTrip />
+        <Route path='/map' component={CreateTrip} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.css'
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
