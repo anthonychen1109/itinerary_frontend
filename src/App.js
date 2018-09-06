c44ae024bad4e7e9a44a23edc8da91b56a373162
jtx007 <jamesjacobthomas7@gmail.com>
2018-09-06 15:09:59 -0400

    put out the fire

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-
-        <CreateTrip />
-=======
         <NavBar />
         <Route path='/map' component={CreateTrip} />
->>>>>>> james-dev
       </div>
     );
   }
