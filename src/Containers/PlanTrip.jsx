0d08ba28e1fab32d1dd6314567f1c72f4ad7f779
github username <github email address>
2018-09-06 14:04:09 -0400

    added calendar

--- src/Containers/PlanTrip.jsx
+++ src/Containers/PlanTrip.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import {Input} from 'antd';
+import Calendar from 'react-calendar';
 
 class PlanTrip extends Component {
+
+  state = {
+    date: new Date()
+  }
+
+  handleChange = (date) => {
+    this.setState({ date })
+  }
+
   render() {
     return (
       <div>
         <form>
           <div className="planTripForm">
-            Starting Location:
-            <Input placeholder="Starting Location" size="small" value={this.props.tripStart} name="tripStart"/>
+            <h3 className="planTripHeaders">
+              Starting Location:
+            </h3>
+            <Input placeholder="Starting Location" size="large" value={this.props.tripStart} name="tripStart"/>
           </div>
           <div>
-            Ending Location:
-            <Input placeholder="Ending Location" size="small" value={this.props.tripEnd} name="tripEnd"/>
+            <h3 className="planTripHeaders">
+              Ending Location:
+            </h3>
+            <Input placeholder="Ending Location" size="large" value={this.props.tripEnd} name="tripEnd"/>
+          </div>
+          <div className="planTripCalendar">
+            <Calendar onChange={this.handleChange} value={this.state.date}/>
           </div>
         </form>
       </div>
