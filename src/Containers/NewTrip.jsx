8d1f151d97436eae5bcc2fdecaa50bb6cf0f01c1
runandrerun <the.asantiagojr@gmail.com>
2018-09-12 11:28:48 -0400

    merging

--- src/Containers/NewTrip.jsx
+++ src/Containers/NewTrip.jsx
@@ -12,17 +12,22 @@ class NewTrip extends Component {
     endingLocation: '',
     tripName: '',
     coordinates: [],
-    currentTrip: '',
     startDate: '',
     endDate: '',
     allTrips: [],
-    currentTrip: {}
+    currentTrip: {},
+    filterLocations: [],
+    relations: []
   }
 
   addStartLocation = (e) => {
     this.setState({
       startingLocation: e.target.value
     })
+  }
+
+  componentDidMount() {
+    this.fetchLocations()
   }
 
   fetchStartLocation = () => {
@@ -168,20 +173,130 @@ class NewTrip extends Component {
       body: JSON.stringify(newTrip)
     })
       .then(res => res.json())
-      .then(console.log)
+      .then(this.createNewLocations)
   }
 
-  // createNewLocations = () => {
-  //   fetch(`http://localhost:3000/trip_locations`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify(newTrip)
-  //   })
-  //     .then(res => res.json())
-  // }
 
+  createNewLocations = () => {
+    // console.log(this.state.destinations)
+    const destinations = this.state.destinations
+    const coordinates = this.state.coordinates
+    const newLocations = [{
+      name: destinations[0],
+      lat: coordinates[0].lat,
+      lng: coordinates[0].lng
+    },
+  {
+    name: destinations[1],
+    lat: coordinates[1].lat,
+    lng: coordinates[1].lng
+  }]
+  // console.log(newLocations)
+  newLocations.forEach(location => {
+    fetch(`http://localhost:3000/locations`, {
+      method: 'POST',
+      headers: {
+        "Accept": 'application/json',
+        "Authorization": localStorage.getItem('token'),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(location)
+    })
+      .then(res => res.json())
+      .then(this.findTrips)
+    })
+  }
+
+  updateTripsRelationships = () => {
+    const destinations = this.state.destinations
+    const fetchLocations = this.state.filterLocations
+    const newRelations = fetchLocations.map(location => {
+      return destinations.map(destination => {
+        if (destination === location.name) {
+          return location
+        }
+      })
+    })
+    console.log("New", newRelations)
+    return newRelations
+
+  }
+
+  findTrips = () => {
+    console.log("Find", this.state.destinations)
+    const destinations = this.state.destinations
+    const fetchLocations = this.state.filterLocations
+    const arr = []
+    for (let i = 0; i < fetchLocations.length; i++) {
+      for (let j = 0; j < destinations.length; j++) {
+        fetchLocations[i].name === destinations[j] ? arr.push(fetchLocations[i]) : null
+        // console.log(fetchLocations[i].name);
+        // console.log(destinations[j]);
+        // console.log(fetchLocations[i].name === destinations[j]);
+      }
+    }
+    this.setRelations(arr)
+
+    // const newRelations = [...this.state.relations, arr]
+    // this.setState({
+    //   relations: newRelations
+    // })
+    // console.log("Found", this.state.relations)
+    // const found = fetchLocations.forEach(location => {
+    //   let newArr = []
+    //     const newlocs = destinations.forEach(destination => {
+    //     if (destination === location.name) {
+    //       return newArr.push(location)
+    //     }
+    //   })
+    //   return newArr
+    // })
+    // console.log("Found", newArr)
+  }
+
+  setRelations = (locations) => {
+
+  locations.forEach(location => {
+    const newRelation = {
+      trip_id: 1,
+      location_id: location.id
+    }
+    console.log(newRelation)
+    fetch(`http://localhost:3000/trip_locations`, {
+      method: 'POST',
+      headers: {
+        "Accept": 'application/json',
+        "Authorization": localStorage.getItem('token'),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newRelation)
+    })
+      .then(res => res.json())
+      .then(console.log)
+  })
+
+    // console.log(locations)
+    // const newRelations = [...this.state.relations, locations]
+    // this.setState({
+    //   relations: newRelations
+    // })
+  }
+
+  fetchLocations = () => {
+    return fetch(`http://localhost:3000/locations`, {
+     "method": "GET",
+     "headers": {
+       "Content-Type": 'application/json',
+       "Accept": 'application/json',
+       "Authorization": localStorage.getItem('token')
+     }
+   })
+    .then(res => res.json())
+    .then(res => this.setState({
+      filterLocations: res
+      })
+    )
+  }
   // END CREATION
 
   deleteCoordinates = () => {
@@ -189,11 +304,18 @@ class NewTrip extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-    console.log(this.props.currentUser);
-=======
-    // console.log(this.state.coordinates);
->>>>>>> 96c5345a9870bd279e322eaf5a69173506068c55
+    // const arr1 = ["bronx"]
+    // const arr2 = ["bronx", "paris", "london", "brooklyn"]
+    // const new_arr = []
+    // for (let i=0; i<arr1.length; i++) {
+    //   for (let j=0; j<arr2.length; j++) {
+    //     if (arr1[i] === arr2[j]) {
+    //       new_arr.push(arr1[i])
+    //     }
+    //   }
+    // }
+    // console.log(new_arr);
+    // console.log(this.state.filterLocations);
     return (
       <div className="createTrip container">
         <div className="planTrip">
